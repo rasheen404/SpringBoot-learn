a0d76cf34849215be8710dc559d62722e1bab3a9
Rasheen <rasheenerror404@gmail.com>
2024-01-26 22:28:37 +0530

    cretaed a Dynamic web using Thymeleaf

---
+++ SpringBoot-Project8/src/test/java/com/example8/SpringBoot/SpringBootProject8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example8.SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProject8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
