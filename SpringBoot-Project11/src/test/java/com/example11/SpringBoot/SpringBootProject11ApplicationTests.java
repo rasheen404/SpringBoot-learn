bf1e03797c4c922b1c94e183c25137ff00e77737
Rasheen <rasheenerror404@gmail.com>
2024-01-27 13:44:43 +0530

    learned about Controller annotation

---
+++ SpringBoot-Project11/src/test/java/com/example11/SpringBoot/SpringBootProject11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example11.SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProject11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
